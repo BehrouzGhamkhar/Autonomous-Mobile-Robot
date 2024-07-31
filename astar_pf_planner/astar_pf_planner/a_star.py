91391e019c583512efa4dad8ef6d6da88ef5a0c9
AmolRTatkari <artatkari@gmail.com>
2024-07-31 09:37:15 +0200

    astar pf planner submitted for the assigment

---
+++ astar_pf_planner/astar_pf_planner/a_star.py
@@ -0,0 +1,222 @@
+import numpy as np
+import cv2
+from heapq import heappop, heappush
+import matplotlib.pyplot as plt
+from typing import List, Tuple
+import rclpy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
+from rclpy.node import Node
+from std_msgs.msg import Header
+import tf_transformations
+from std_msgs.msg import String
+import math
+
+
+def load_pgm(pgm_file):
+    with open(pgm_file, 'rb') as pgmf:
+        img = plt.imread(pgmf)
+
+        # plt.imshow(np.array(img))
+        # plt.show()
+
+        return np.array(img)
+
+
+def visualize_path(grid, path, output_image_path):
+    path_image = cv2.cvtColor(grid, cv2.COLOR_GRAY2BGR)
+    for point in path:
+        path_image[point[0], point[1]] = (0, 50, 255)  # (B,G,R) orange
+    cv2.imwrite(output_image_path, path_image)
+
+
+class AStarNode:
+    def __init__(self, state: Tuple, g: int, f: int, parent=None):
+        self.state = state
+        self.g = g  # Cost to reach this state
+        self.f = f  # Estimated total cost. (g + h) for A* and h for best first search
+        self.parent = parent  # Parent node
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+
+class AStarPathPlanner(Node):
+    def __init__(self, node_name: str, pgm_file, map_scale, start, goal, min_threshold, output_image_path, grid_pivot):
+        super().__init__(node_name)
+        self.goal_pose_sub = self.create_subscription(PoseStamped, "/goal_pose", self.goal_callback, 10)
+        self.path_pub = self.create_publisher(Path, "/path", 10)
+        # self.path_y_pub = self.create_publisher(Float32MultiArray, "/path_y", 10)
+        self.pgm_file_path = pgm_file
+        self.start_pose = start
+        self.goal_pose = goal
+        self.min_threshold = min_threshold
+        self.output_image_path = output_image_path
+        self.astar_path = []
+        self.map_scale = map_scale
+        self.grid_pivot = grid_pivot
+
+    def goal_callback(self, data):
+
+        grid = load_pgm(self.pgm_file_path)
+
+        self.get_logger().info(f'Grid Shape:{grid.shape}, Grid Length:{len(grid)}')
+
+        pose_stamp = data.pose.position
+
+        # rescale the sim goal_pose to pgm map scales
+        self.goal_pose = (math.floor(len(grid) - ((pose_stamp.y - self.grid_pivot[1]) * self.map_scale)),
+                          math.floor((pose_stamp.x - self.grid_pivot[0]) * self.map_scale))
+        
+        self.get_logger().info(f'Goal_pose in grid:{self.goal_pose}')
+
+        # calculate remainder
+        #self.grid_pivot[1] += ((len(grid) - (pose_stamp.y - self.grid_pivot[1]) * self.map_scale) % 1) / self.map_scale
+        #self.grid_pivot[0] += (((pose_stamp.x - self.grid_pivot[0]) * self.map_scale) % 1) / self.map_scale
+
+        self.get_logger().info(f"Getting goal pose: {pose_stamp}")
+
+        self.plan(grid)
+
+    def heuristic(self, current, goal):
+        # manhattan distance
+        return abs(current[0] - goal[0]) + abs(current[1] - goal[1])
+
+    def check_threshold(self, grid, new_position):
+        len_x = len(grid)
+        len_y = len(grid[:][0])
+
+        for i in range(self.min_threshold + 1):
+            if new_position[0] + i > len_x:
+                return False
+            if new_position[1] + i > len_y:
+                return False
+            if new_position[0] - i < 0:
+                return False
+            if new_position[1] - i < 0:
+                return False
+
+        for i in range(-self.min_threshold, self.min_threshold + 1):
+            for j in range(-self.min_threshold, self.min_threshold + 1):
+                if grid[new_position[0] + i][new_position[1] + j] < 254.0:
+                    return False
+
+        return True
+
+    def get_possible_moves(self, grid, current: Tuple, min_threshold: int = 0) -> List[Tuple]:
+        possible_moves = []
+        neighbors = [(i, j) for i in range(-1, 2) for j in range(-1, 2)]
+
+        for neighbor in neighbors:
+            new_position = (current[0] + neighbor[0], current[1] + neighbor[1])
+            if grid[new_position[0]][new_position[1]] >= 254.0:
+                if self.check_threshold(grid, new_position):
+                    possible_moves.append(new_position)
+
+        return possible_moves
+
+    def astar(self, grid, start, goal, min_threshold) -> Tuple[List[int], int]:
+        explored_set = set()
+        start_node = AStarNode(start, 0, self.heuristic(start, goal))
+        fringe = [start_node]
+
+        while fringe:
+            current_node = heappop(fringe)
+            current_state = current_node.state
+
+            if current_state == goal:
+                # Reconstruct the solution path
+                solution_path = [current_state]
+                while current_node.parent:
+                    current_node = current_node.parent
+                    solution_path.append(current_node.state)
+                solution_path.reverse()
+                return solution_path, len(explored_set)
+
+            explored_set.add(current_state)
+
+            for new_state in self.get_possible_moves(grid, current_state, min_threshold):
+                if new_state not in explored_set:
+                    new_g = current_node.g + 1
+                    new_f = new_g + self.heuristic(new_state, goal)
+                    new_node = AStarNode(new_state, new_g, new_f, parent=current_node)
+                    heappush(fringe, new_node)
+
+        return [], len(explored_set)
+
+    def astarpath_to_rospath(self, astar_path, grid):
+
+        path = Path()
+        path.header = Header()
+        path.header.stamp = self.get_clock().now().to_msg()
+        path.header.frame_id = "odom"
+
+        for i, point in enumerate(astar_path):
+            pose = PoseStamped()
+            pose.header = Header()
+            pose.header.stamp = self.get_clock().now().to_msg()
+            pose.header.frame_id = 'odom'
+
+            # rescale points back to sim scale
+            pose.pose.position.x = ((point[1]) / self.map_scale) + self.grid_pivot[0]
+            pose.pose.position.y = ((len(grid) - point[0]) / self.map_scale) + self.grid_pivot[1]
+
+            pose.pose.position.z = 0.0  # Assuming a 2D path
+
+            if i < len(astar_path) - 1:  # Calculate orientation for all points except the last one
+                next_point = astar_path[i + 1]
+                dx = next_point[0] - point[0]
+                dy = next_point[1] - point[1]
+                yaw = math.atan2(dy, dx)
+                quaternion = tf_transformations.quaternion_from_euler(0, 0, yaw)
+                pose.pose.orientation.x = quaternion[0]
+                pose.pose.orientation.y = quaternion[1]
+                pose.pose.orientation.z = quaternion[2]
+                pose.pose.orientation.w = quaternion[3]
+            else:  # For the last point, use the default orientation
+                pose.pose.orientation.z = 0.0
+                pose.pose.orientation.w = 1.0
+
+            path.poses.append(pose)
+
+        return path
+
+    def plan(self, grid):
+        if not self.check_threshold(grid, self.goal_pose):
+            self.get_logger().info(f"Goal Pose is not valid!")
+            return False
+
+        astar_path, _ = self.astar(grid, self.start_pose, self.goal_pose, self.min_threshold)
+        print(astar_path)
+        self.astar_path = astar_path
+        if astar_path:
+            path = self.astarpath_to_rospath(astar_path[::10], grid)
+            #path = self.astarpath_to_rospath(astar_path, grid)
+            self.path_pub.publish(path)
+
+            visualize_path(grid, astar_path, self.output_image_path)
+            self.get_logger().info(f"A star path: {self.astar_path}")
+
+        else:
+            self.get_logger().info(f"A star path: path not found!")
+
+
+def main(args=None):
+    pgm_file = 'src/Autonomous-Mobile-Robot/astar_pf_planner/astar_pf_planner/maps/closed_walls_map.pgm'
+    output_image_path = 'path_outputs/map_A_star_path_planning.jpg'
+   # grid_pivot = [-0.806, -4.85]
+    grid_pivot = [-0.806, -4.85]
+    start = (38, 16)
+    goal = (100, 100)
+    min_threshold = 12
+    map_scale = 20.0
+    rclpy.init(args=args)
+    sub = AStarPathPlanner("path_planner", pgm_file, map_scale, start, goal, min_threshold, output_image_path,
+                           grid_pivot)
+    rclpy.spin(sub)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ astar_pf_planner/astar_pf_planner/potential_field_planner.py
@@ -0,0 +1,346 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from rclpy.serialization import deserialize_message
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import PoseStamped, Twist
+from tf2_msgs.msg import TFMessage
+import numpy as np
+import math
+import tf_transformations, tf2_ros, tf2_geometry_msgs
+from tf2_geometry_msgs.tf2_geometry_msgs import do_transform_pose
+from nav_msgs.msg import Odometry, Path
+
+
+class Subscriber(Node):
+    def __init__(self, node_name: str):
+        super().__init__(node_name)
+        self.goal_pose=PoseStamped()
+        self.goal_pose_laser_link=PoseStamped()
+        self.goal_reached=False
+
+        self.curr_pose=PoseStamped()
+        self.curr_pose_laser_link=PoseStamped()
+        self.goal_pose_received=False
+        self.path=[]
+
+        self.nearest_path_index=0
+        self.goal_index=0
+
+        self.env_data=[]
+        self.k_attraction = -0.5
+        self.k_replusion = 0.5
+        self.threshold_dist = 0.7
+
+        self.laser_scan_sub = self.create_subscription(LaserScan, "/scan", self.laserCallback, 10)
+        self.goal_pose_sub = self.create_subscription(Path, "/path", self.goalPoseCallback, 10)
+        #self.goal_pose_sub = self.create_subscription(PoseStamped, "/goal_pose", self.goalPoseCallback, 10)
+        self.curr_pose_sub = self.create_subscription(Odometry,"/odom",self.odom_callback,10)
+
+        self.pub_cmd_vel=self.create_publisher(Twist,"/cmd_vel",10)
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
+        
+    def odom_callback(self,msg):
+
+        self.curr_pose=msg.pose.pose 
+
+        #Transforming current pose from 'odom' to 'base_laser_front_link'
+        self.curr_pose_laser_link.header.frame_id='base_laser_front_link'
+        if self.curr_pose is None:
+            self.get_logger().info('Goal pose not received yet.')
+            return
+        
+        try:
+            trans = self.tf_buffer.lookup_transform('base_laser_front_link', 'odom', rclpy.time.Time())
+
+            self.curr_pose_laser_link.pose = do_transform_pose(self.curr_pose, trans)
+
+
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            self.get_logger().warn("Failed to transform current pose to base_laser_front_ink frame")
+            return
+
+
+        #Selecting nearest point on the path to current pose
+        individual_pt=PoseStamped()
+        
+        if(len(self.path)==0):
+            print("Path is not received")
+        else:
+            print("Path is received")
+
+            for i,poses in enumerate(self.path):
+                
+                #self.get_logger().info(f'Nearest point on path is {self.curr_pose.position.x-poses.pose.pose.position.x}')
+                
+                if abs(self.curr_pose.position.x-poses.pose.position.x)<0.06  and abs(self.curr_pose.position.y-poses.pose.position.y)<0.06:
+                    self.nearest_path_index=i
+                    #self.get_logger().info(f'Nearest point on path is {poses.position}')
+                    self.get_logger().info(f'Nearest index is {self.nearest_path_index}')
+
+                '''
+                individual_pt=self.path[1]
+                if self.curr_pose.position==individual_pt.pose.position:
+                    print("starting point is at zero")
+                else:
+                    print("starting point is not at zero")
+                '''
+            print(f"Goal Index is:{self.nearest_path_index+1}")
+
+            
+            if (self.goal_index)==(len(self.path)-1):
+                self.goal_index=len(self.path)-1
+            else:
+                self.goal_index=self.nearest_path_index+1
+
+            individual_pt=self.path[self.goal_index]
+
+            self.goal_pose=individual_pt.pose
+            #self.get_logger().info(f'Goal_pt is {self.goal_pose}')
+
+            self.goal_pose_laser_link.header.frame_id='base_laser_front_link'
+            if self.goal_pose is None:
+                self.get_logger().info('Goal pose not received yet.')
+                return
+            
+            try:
+                trans = self.tf_buffer.lookup_transform('base_laser_front_link', 'odom', rclpy.time.Time())
+                self.goal_pose_laser_link.pose = do_transform_pose(self.goal_pose, trans)
+
+
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                self.get_logger().warn("Failed to transform goal pose to base_laser_front_ink frame")
+                return
+            
+            self.get_logger().info(f'Goal_pt after transform is {self.goal_pose_laser_link.pose}')
+        
+    def goalPoseCallback(self, msg):
+
+        self.goal_pose_received=True
+
+        self.path=msg.poses
+        '''
+        if (msg.header.frame_id!='odom'):
+            self.get_logger().warn(f"Path is not in odom frame!")
+        #below I am assigning only second point to the goal. This was part of initial testing
+        #This is the problem. I need to constantly receive the goal_pose as I am using goal_pose w.r.t. base_laser_link which is moving w.r.t. odom frame
+        self.goal_pose=self.path[1].pose
+        self.get_logger().warn(f"First transformed goal is:{self.goal_pose}")
+        self.goal_pose_laser_link.header.frame_id='base_laser_front_link'
+        if self.goal_pose is None:
+            self.get_logger().info('Goal pose not received yet.')
+            return
+        
+        try:
+            trans = self.tf_buffer.lookup_transform('base_laser_front_link', 'odom', rclpy.time.Time())
+            self.goal_pose_laser_link.pose = do_transform_pose(self.goal_pose, trans)
+
+
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            self.get_logger().warn("Failed to transform goal pose to base_laser_front_ink frame")
+            return
+        
+        #self.get_logger().warn(f"First transformed goal is:{ self.goal_pose_laser_link.pose.position}")
+        '''
+    
+        
+    def calc_rep_force_x(self,data):
+
+        force=self.k_replusion*(((1/data[1])-(1/self.threshold_dist))*(0-data[2])/(data[1]*data[1]*data[1]))
+
+        return force
+    
+    def calc_rep_force_y(self,data):
+        
+        force=self.k_replusion*(((1/data[1])-(1/self.threshold_dist))*(0-data[3])/(data[1]*data[1]*data[1]))
+        return force
+    
+
+    def calc_attractive_force_x(self):
+
+        dx=self.curr_pose_laser_link.pose.position.x-self.goal_pose_laser_link.pose.position.x
+        dy=self.curr_pose_laser_link.pose.position.y-self.goal_pose_laser_link.pose.position.y
+        force=0
+
+        dist_to_goal=math.sqrt(dx**2 + dy**2)
+
+
+        if dist_to_goal>0.05 and self.goal_pose_received==True:
+            force=self.k_attraction*(dx)/dist_to_goal
+        else:
+            force=0.0
+
+        if dist_to_goal<0.05 and self.goal_pose_received==True:
+            self.goal_reached=True
+        else:
+            self.goal_reached=False
+        return force
+    
+    def calc_attractive_force_y(self):
+        dx=self.curr_pose_laser_link.pose.position.x-self.goal_pose_laser_link.pose.position.x
+        dy=self.curr_pose_laser_link.pose.position.y-self.goal_pose_laser_link.pose.position.y
+        force=0
+
+        dist_to_goal=math.sqrt(dx**2 + dy**2)
+
+        if dist_to_goal>0.05 and self.goal_pose_received==True:
+            force=self.k_attraction*(dy)/dist_to_goal
+
+        else:
+            force=0.0
+
+        if dist_to_goal<0.05 and self.goal_pose_received==True:
+            self.goal_reached=True
+        else:
+            self.goal_reached=False
+        
+        return force
+
+
+    def laserCallback(self, msg):
+
+        self.env_data.clear()
+        np_polar_array = self.extract_ranges(msg)
+        np_cart_array = self.np_polar2cart(np_polar_array)
+
+        #Now I am storing environment data as [angle,range,cartesian_x, cartesian_y]
+        angle_increment = msg.angle_increment
+        angle_min = msg.angle_min
+
+        for i,distance in enumerate(msg.ranges):
+            self.env_data.append([angle_min + i * angle_increment,distance,np_cart_array[i][0],np_cart_array[i][1]])
+
+        #self.get_logger().info(f'Obstacle_data:{(msg.ranges)}')
+        obstacle_points=[]
+        rep_force_x=[]
+        rep_force_y=[]
+        rep_force_x_overall=[]        
+        rep_force_y_overall=[]
+        index_start = -1
+        index_end = -1
+        found_start = False
+        for i, data in enumerate(self.env_data):
+            if math.isinf(data[1]):
+                if found_start == True:
+                    index_end=i
+                    found_start = False
+                    obstacle_points.append(self.env_data[index_start:index_end])
+                pass
+            else:
+                if found_start == False:
+                    index_start=i
+                    found_start = True
+        
+        #Clustering lidar(i.e. obstacle points). Here neighbouring points are clustered into one group and repulsive force corresponding to each point is calculated
+        for i in obstacle_points:
+            rep_force_x = []
+            rep_force_y=[]
+
+            for j in i:
+                rep_force_x.append(self.calc_rep_force_x(j))
+                rep_force_y.append(self.calc_rep_force_y(j))
+
+            rep_force_x_overall.append(rep_force_x)
+            rep_force_y_overall.append(rep_force_y)
+            
+        average_rep_force_x=[]
+        average_rep_force_y=[]
+
+        #In this algorithm, addition and subtraction of the attractive and repulsive forces is done at base_laser_link
+        
+        #Calculating average repulsive force in X direction. This average is for one cluster
+        for count, data1 in enumerate(rep_force_x_overall):
+            average_rep_force_x.append(sum(data1)/len(data1))
+        
+        #Calculating average repulsive force in Y direction. This average is for one cluster
+        for int, data2 in enumerate(rep_force_y_overall):
+            average_rep_force_y.append(sum(data2)/len(data2))
+
+        rep_force_data=[]
+        
+        for i in range(len(average_rep_force_x)):
+            rep_force_data.append([average_rep_force_x[i],average_rep_force_y[i]])
+
+        #Calculating attractive forces in 
+        attr_force_x=self.calc_attractive_force_x()
+        attr_force_y=self.calc_attractive_force_y()
+
+        velocity_x=0.0
+        velocity_y=0.0
+        
+        #Adding repulsive forces
+        for force_x, force_y in rep_force_data:
+        
+            velocity_x=velocity_x+force_x
+            velocity_y=velocity_y+force_y
+        
+
+        #Adding the attractive force
+        if self.goal_pose_received==False or self.goal_reached==True:
+            velocity_x=0.0
+            velocity_y=0.0          
+        else:    
+            velocity_x=velocity_x+attr_force_x
+            velocity_y=velocity_y+attr_force_y
+
+        #Capping the velocity
+        if(velocity_x>1.0):
+            velocity_x=0.5
+
+        if(velocity_y>0.5):
+            velocity_y=0.2
+
+
+        #As addition and subtraction of forces is done at the base_laser_front_link, I am converting corrsponding velocities to base_link. 
+        base_link_x_vel=velocity_x
+        base_link_WZ_vel=velocity_y/0.45
+
+        #Publishing velocity data into cmd_vel
+        vel_cmd=Twist()
+        vel_cmd.linear.x=base_link_x_vel
+        vel_cmd.angular.z=base_link_WZ_vel
+
+        self.pub_cmd_vel.publish(vel_cmd)
+      
+    def extract_ranges(self, msg):
+        m_array = []
+        # calculate Mikel Arteta
+        angle_increment = msg.angle_increment
+        angle_min = msg.angle_min
+        for index, distance in enumerate(msg.ranges):
+            m_array.append([distance, angle_min + index * angle_increment])
+
+        np_array = np.array(m_array)
+        return np_array
+
+    def np_polar2cart(self, np_polar):
+        """
+        np_polar: 2-dimenisional numpy array, where each row consists of the distance to the obstacle and the
+                corresponding angle in radians. Thus, number of rows will be equal to the number of scan data points
+        Return:
+        np_cart: 2-dimenisional numpy array, where each row consists of X and Y coordinates in Cartesian system
+        """
+        np_cart = []
+
+        for i, (radius, angle) in enumerate(np_polar):
+            coordinates = [radius * math.sin(angle), radius * math.cos(angle)]
+            np_cart.append(coordinates)
+
+            # test if it is working: length = sqrt(x^2 + y^2)
+            # if (math.isclose(math.sqrt(coordinates[0] ** 2 + coordinates[1] ** 2), radius)):
+            #    print("True")
+
+        return np.array(np_cart)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    sub = Subscriber("pf_planner")
+    rclpy.spin(sub)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
